e266bbd66388e7bc392925d032fc4e77b10b2045
Raphael Vieira <raphael@localhost.localdomain>
2019-06-29 13:48:07 -0400

    *chore: add cryptography to password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // Field only exists on code, does not exists on table
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +15,14 @@ class User extends Model {
         sequelize,
       }
     );
+
+    // Hook - this is an piece of code that is executed on the action setted on the first parameter
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8); // strengh of cryptography
+      }
+    });
+    return this;
   }
 }
 
