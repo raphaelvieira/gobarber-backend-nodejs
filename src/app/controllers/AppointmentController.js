6c2912bd002a89958a9ea823e9117cb5322d5a3e
Raphael Vieira <raphael@localhost.localdomain>
2019-06-30 20:16:16 -0400

    *chore List Appointments Pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,12 +6,15 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
         cancelled_at: null,
       },
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       order: ['date'],
       include: [
         {
