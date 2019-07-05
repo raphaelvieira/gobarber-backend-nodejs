7269cc3c31006e87cd2e367deb085fa00e5e75d4
Raphael Vieira <raphael@localhost.localdomain>
2019-07-04 22:01:24 -0400

    *chore Availability

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,16 +1,65 @@
+import {
+  startOfDay,
+  endOfDay,
+  setSeconds,
+  setHours,
+  setMinutes,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
 
-class AvailableController{
-  async index(req, res){
-    const {date } = req.query;
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
 
-    if(!date){
-      return res.status(400).json({ error: 'Invalid date'})
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
     }
 
     const searchDate = Number(date);
 
+    const appointments = await Appointment.findAll({
+      where: {
+        date: { [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)] },
+        cancelled_at: null,
+        provider_id: req.params.providerId,
+      },
+    });
 
-    return res.json();
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      // set format HH:MM:SS to the times
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
   }
 }
 
