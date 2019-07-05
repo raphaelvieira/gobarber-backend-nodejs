ab1fda89b1c1541a8bd8b4bd7a1b313a8eb0efb9
Raphael Vieira <raphael@localhost.localdomain>
2019-07-05 00:45:37 -0300

    *feature Handler Exception on code and Sentry configuration

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+// need to import before routes
+import 'express-async-errors';
 import routes from './routes';
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
-
     // used for allow access to the folder files
     this.server.use(
       '/files',
@@ -20,8 +30,17 @@ class App {
     );
   }
 
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, err).toJSON();
+      return res.status(500).json(errors);
+    });
+  }
+
   routes() {
     this.server.use(routes);
+    // The error handler must be before any other error middleware and after all controllers
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d5b3b68ececb4fdeb32d3845e0d5e26d@sentry.io/1497547',
+};
